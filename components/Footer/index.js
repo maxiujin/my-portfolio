6daf2d7dca37603ce37067e187e7f467dd193361
Matthew Thompkins <alkeccg@gmail.com>
2023-10-17 15:29:30 -0400

    portfolio

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -24,10 +24,10 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
+        {/* Made With ❤ by{" "}
         <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
-        </Link>
+          <a className="underline underline-offset-1">Chetan Verm</a>
+        </Link> */}
       </h1>
     </>
   );
